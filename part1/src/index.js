bb34aa04c2b4f8245269419a0593acae5ca64842
Jason Linares <jason.linares@outlook.com>
2021-12-31 00:09:15 -0300

    Completed until part 1d

--- part1/src/index.js
+++ part1/src/index.js
@@ -1,11 +1,33 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable react/destructuring-assignment */
+/* eslint-disable react/prop-types */
 /* eslint-disable react/jsx-filename-extension */
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import Content from './components/Content';
+import Display from './components/Display';
 import Header from './components/Header';
 import Total from './components/Total';
+import Button from './components/Button';
+
+function Hello({ name, age }) {
+  const bornYear = () => new Date().getFullYear() - age;
+
+  return (
+    <div>
+      <p>{`Hello ${name}, you are ${age} years old`}</p>
+      <p>{`So you were probably born in ${bornYear()}`}</p>
+    </div>
+  );
+}
 
 function App() {
+  const [counter, setCounter] = useState(0);
+
+  const increaseByOne = () => setCounter(counter + 1);
+  const decreaseByOne = () => setCounter(counter - 1);
+  const setToZero = () => setCounter(0);
+
   const course = {
     name: 'Half Stack application development',
     parts: [
@@ -24,11 +46,17 @@ function App() {
     ],
   };
 
+  const name = 'Peter';
+  const age = 10;
+
   return (
     <div>
-      <Header course={course.name} />
-      <Content parts={course.parts} />
-      <Total parts={course.parts} />
+      <h1>Greetings</h1>
+      <Display counter={counter} />
+      <Button handleClick={increaseByOne} text="plus" />
+      <Button handleClick={decreaseByOne} text="minus" />
+      <Button handleClick={setToZero} text="zero" />
+
     </div>
   );
 }
