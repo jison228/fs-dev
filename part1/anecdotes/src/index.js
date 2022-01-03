358283cb148d2647ef91f9f6bd740eff3957b53f
Jason Linares <jason.linares@outlook.com>
2022-01-02 22:02:29 -0300

    Finished anecdotes

--- part1/anecdotes/src/index.js
+++ part1/anecdotes/src/index.js
@@ -1,30 +1,58 @@
-/* eslint-disable no-unused-vars */
+/* eslint-disable prefer-spread */
+/* eslint-disable no-plusplus */
 /* eslint-disable react/jsx-filename-extension */
+/* eslint-disable react/prop-types */
 import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
-import Statistics from './components/Statistics';
 import Button from './components/Button';
 
-function App() {
-  // save clicks of each button to its own state
-  const [good, setGood] = useState(0);
-  const [neutral, setNeutral] = useState(0);
-  const [bad, setBad] = useState(0);
-  const stats = { Good: good, Neutral: neutral, Bad: bad };
+function randomIntFromInterval(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
 
+function App({ anecdotes }) {
+  const [selected, setSelected] = useState(0);
+  const [votes, setVotes] = useState({});
+  const max = Math.max.apply(Math, Object.values(votes));
+  let maxElem = null;
+  if (Object.values(votes).length > 0) {
+    maxElem = Object.keys(votes)[Object.values(votes).findIndex((el) => el === max)];
+  }
   return (
     <div>
-      <h1>Give feedback</h1>
-      <Button text="Good" handleClick={() => { setGood(good + 1); }} />
-      <Button text="Neutral" handleClick={() => { setNeutral(neutral + 1); }} />
-      <Button text="Bad" handleClick={() => { setBad(bad + 1); }} />
-      <h1>Statistics</h1>
-      <Statistics stats={stats} />
+      <h1>Anecdote of the day</h1>
+      <p>{anecdotes[selected]}</p>
+      <Button
+        text="Vote"
+        handleClick={() => {
+          setVotes((prevState) => {
+            const prevStatecopy = { ...prevState };
+            if (prevStatecopy[selected]) {
+              prevStatecopy[selected] += 1;
+            } else {
+              prevStatecopy[selected] = 1;
+            }
+            return prevStatecopy;
+          });
+        }}
+      />
+      <Button text="Next Anecdote" handleClick={() => { setSelected(randomIntFromInterval(0, anecdotes.length - 1)); }} />
+      <h1>Anecdote with most votes</h1>
+      <p>{Object.values(anecdotes)[maxElem] ? Object.values(anecdotes)[maxElem] : ''}</p>
     </div>
   );
 }
 
+const anecdotes = [
+  'If it hurts, do it more often',
+  'Adding manpower to a late software project makes it later!',
+  'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+  'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+  'Premature optimization is the root of all evil.',
+  'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+];
+
 ReactDOM.render(
-  <App />,
+  <App anecdotes={anecdotes} />,
   document.getElementById('root'),
 );
